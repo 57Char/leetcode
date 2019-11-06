71afa36d1264612a02fd7569b7130d1612421606
yutao52 <yutao12@jd.com>
2019-11-06 17:14:34 +0800

    init rotate-array

---
+++ algorithms/rotate-array.go
@@ -0,0 +1,38 @@
+package algorithms
+
+// https://leetcode.com/problems/rotate-array/description/
+
+func rotate(nums []int, k int) {
+	l := len(nums)
+	if l <= 1 || k <= 0 {
+		return
+	}
+
+	k = k % l
+	if k == 0 {
+		return
+	}
+
+	for i := 0; i < k; i++ {
+		for j := l - 1; j > 0; j-- {
+			nums[j], nums[j-1] = nums[j-1], nums[j]
+		}
+	}
+}
+
+func rotateV2(nums []int, k int) {
+	l := len(nums)
+	if l <= 1 || k <= 0 {
+		return
+	}
+
+	k = k % l
+	if k == 0 {
+		return
+	}
+
+	newNums := make([]int, l)
+	copy(newNums[:k], nums[l-k:])
+	copy(newNums[k:], nums[:l-k])
+	copy(nums, newNums)
+}
---
+++ algorithms/rotate-array_test.go
@@ -0,0 +1,115 @@
+package algorithms
+
+import (
+	"testing"
+)
+
+import (
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRotate(t *testing.T) {
+	cases := []struct {
+		Name   string
+		K      int
+		Input  []int
+		Expect []int
+	}{
+		{
+			"Empty Input",
+			10,
+			[]int{},
+			[]int{},
+		},
+		{
+			"Empty K",
+			0,
+			[]int{1, 2, 3},
+			[]int{1, 2, 3},
+		},
+		{
+			"Len(Input) == K",
+			3,
+			[]int{1, 2, 3},
+			[]int{1, 2, 3},
+		},
+		{
+			"Len(Input) * 2 == K",
+			6,
+			[]int{1, 2, 3},
+			[]int{1, 2, 3},
+		},
+		{
+			"Len(Input) > K",
+			3,
+			[]int{1, 2, 3, 4, 5, 6, 7},
+			[]int{5, 6, 7, 1, 2, 3, 4},
+		},
+		{
+			"Len(Input) < K",
+			8,
+			[]int{1, 2, 3, 4, 5, 6, 7},
+			[]int{7, 1, 2, 3, 4, 5, 6},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			rotate(test.Input, test.K)
+			assert.Equal(t, test.Expect, test.Input)
+		})
+	}
+}
+
+func TestRotateV2(t *testing.T) {
+	cases := []struct {
+		Name   string
+		K      int
+		Input  []int
+		Expect []int
+	}{
+		{
+			"Empty Input",
+			10,
+			[]int{},
+			[]int{},
+		},
+		{
+			"Empty K",
+			0,
+			[]int{1, 2, 3},
+			[]int{1, 2, 3},
+		},
+		{
+			"Len(Input) == K",
+			3,
+			[]int{1, 2, 3},
+			[]int{1, 2, 3},
+		},
+		{
+			"Len(Input) * 2 == K",
+			6,
+			[]int{1, 2, 3},
+			[]int{1, 2, 3},
+		},
+		{
+			"Len(Input) > K",
+			3,
+			[]int{1, 2, 3, 4, 5, 6, 7},
+			[]int{5, 6, 7, 1, 2, 3, 4},
+		},
+		{
+			"Len(Input) < K",
+			8,
+			[]int{1, 2, 3, 4, 5, 6, 7},
+			[]int{7, 1, 2, 3, 4, 5, 6},
+		},
+	}
+
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			rotateV2(test.Input, test.K)
+			assert.Equal(t, test.Expect, test.Input)
+		})
+	}
+}
